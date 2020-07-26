7f97882328ad77b09cf2171c84fa68fd930037a9
ZOUBA\Waref <hani.waref@gmail.com>
2020-07-26 23:21:40 +0200

    Add swagger URL and set Responce as JSON instead of XML

--- WarefDemo/App_Start/WebApiConfig.cs
+++ WarefDemo/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace WarefDemo
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            // Remove the XML formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
